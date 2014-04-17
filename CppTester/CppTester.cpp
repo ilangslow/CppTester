3c97b91ae48a420e1f96783fd480d25234a246c9
John <John.Smith@email.com>
2014-04-17 11:46:32 +0100

    Third commit comment
    A change to the .gitignore file plus further work on the main cpp file.

--- CppTester/CppTester.cpp
+++ CppTester/CppTester.cpp
@@ -11,12 +11,28 @@
 using std::cout;
 using std::endl;
 
+class Foo
+{
+public:
+	Foo() : m_value(0) {}
+
+	Foo(const int value) : m_value(value) {}
+
+	int getValue() { return m_value; }
+
+private:
+	int m_value;
+};
+
 int _tmain(int argc, _TCHAR* argv[])
 {
 	cout << "Starting ..." << endl;
 
 	CPPLibInterface::SimpleExportExample see;
 	cout << "SimpleExportExample has value: " << see.getValue() << endl;
+
+	Foo foo(111);
+	cout << "foo has value: " << foo.getValue() << endl;
 
 	cout << "Exiting ..." << endl;
 
