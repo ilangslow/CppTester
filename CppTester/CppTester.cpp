fa7de915bcd69ecac06c91dc3d79f9b4cfcca7ab
John <John.Smith@email.com>
2014-04-17 11:17:18 +0100

    Second commit comment
    Longer explaination of the second commit comment goes here.

--- CppTester/CppTester.cpp
+++ CppTester/CppTester.cpp
@@ -6,12 +6,17 @@
 #include <iostream>
 #include <vector>
 
+#include "../TestLib/LibInterface.h"
+
 using std::cout;
 using std::endl;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	cout << "Starting ..." << endl;
+
+	CPPLibInterface::SimpleExportExample see;
+	cout << "SimpleExportExample has value: " << see.getValue() << endl;
 
 	cout << "Exiting ..." << endl;
 
